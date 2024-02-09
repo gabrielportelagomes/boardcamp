603f7a5851836abaac8ed3a0dd817c7e467254b4
Gabriel Portela Gomes <gabriel@flowdrive.co>
2024-02-08 23:35:03 -0300

    feat: first commit

---
+++ src/test/java/com/example/boardcamp/BoardcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
